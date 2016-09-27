13c070618e181a66171f6c30a8c52713f05e7905
Nate Jenson <nate.m.jenson@gmail.com>
2016-09-26 21:10:50 -0500

    Add Xunit unit tests

---
+++ test/TodoApi.Tests/MockTodoRepository.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using TodoApi.Models;
+
+namespace TodoApi.Tests
+{
+	internal class MockTodoRepository : ITodoRepository
+	{
+		public ConcurrentDictionary<string, TodoItem> Todos;
+
+		public MockTodoRepository()
+		{
+			Todos =
+				new ConcurrentDictionary<string, TodoItem>(
+					GetTestTodoItems().Select(t => new KeyValuePair<string, TodoItem>(t.Key, t)));
+		}
+
+		public void Add(TodoItem item)
+		{
+			Todos.TryAdd(Guid.NewGuid().ToString(), item);
+		}
+
+		public void Update(TodoItem item)
+		{
+			Todos[item.Key] = item;
+		}
+
+		public IEnumerable<TodoItem> GetAll()
+		{
+			return Todos.Values;
+		}
+
+		public TodoItem Find(string key)
+		{
+			TodoItem todo;
+			Todos.TryGetValue(key, out todo);
+			return todo;
+		}
+
+		public TodoItem Remove(string key)
+		{
+			TodoItem todo;
+			Todos.TryRemove(key, out todo);
+			return todo;
+		}
+
+		public static IEnumerable<TodoItem> GetTestTodoItems()
+		{
+			return new []
+			{
+				new TodoItem { Key = "1", Name = "Go for a bike ride", IsComplete = true },
+				new TodoItem { Key = "2", Name = "Read the Iliad", IsComplete = false },
+				new TodoItem { Key = "3", Name = "Brew some beer", IsComplete = true },
+				new TodoItem { Key = "4", Name = "Go hiking", IsComplete = false },
+				new TodoItem { Key = "5", Name = "Adopt a puppy", IsComplete = false },
+				new TodoItem { Key = "6", Name = "Build a .Net Core API", IsComplete = true }
+			};
+		} 
+	}
+}
---
+++ test/TodoApi.Tests/TodoControllerTests.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using TodoApi.Controllers;
+using TodoApi.Models;
+using Xunit;
+
+namespace TodoApi.Tests
+{
+	public class TodoControllerTests
+	{
+		public static IEnumerable<object[]> MismatchedKeyTodos => new[]
+		{
+			new object[] { "key1", new TodoItem {Key = "keyOne"} },
+			new object[] { "foo", new TodoItem {Key = "bar"} },
+			new object[] { "a", new TodoItem {Key = "A"} }
+		};
+
+		public static IEnumerable<object[]> MissingKeyTodos => new[]
+		{
+			new object[] { "nope", new TodoItem {Key = "nope"} },
+			new object[] { "foo", new TodoItem {Key = "foo"} },
+			new object[] { "bar", new TodoItem {Key = "bar"} }
+		};
+
+		[Fact]
+		public void GetAll_ShouldReturnAllTodos()
+		{
+			var todoRepository = new MockTodoRepository();
+			var todoController = new TodoController(todoRepository);
+
+			Assert.Equal(todoRepository.Todos.Values, todoController.GetAll());
+		}
+
+		[Theory]
+		[InlineData("-1")]
+		[InlineData("asdf")]
+		[InlineData("foobar")]
+		public void GetById_WithWrongId_ShouldReturnNotFound(string key)
+		{
+			var todoRepository = new MockTodoRepository();
+			var todoController = new TodoController(todoRepository);
+
+			Assert.IsType(typeof(NotFoundResult), todoController.GetById(key));
+		}
+
+		[Theory]
+		[InlineData("1")]
+		[InlineData("2")]
+		[InlineData("3")]
+		public void GetById_WithGoodId_ShouldReturnTodo(string key)
+		{
+			var todoRepository = new MockTodoRepository();
+			var todoController = new TodoController(todoRepository);
+
+			var result = todoController.GetById(key) as ObjectResult;
+			Assert.NotNull(result); 
+			Assert.IsType(typeof(TodoItem), result.Value);
+		}
+
+		[Fact]
+		public void Create_WithNullTodo_ShouldReturnBadRequest()
+		{
+			var todoRepository = new MockTodoRepository();
+			var todoController = new TodoController(todoRepository);
+
+			var result = todoController.Create(null);
+			Assert.IsType(typeof (BadRequestResult), result);
+		}
+
+		[Theory, MemberData(nameof(MismatchedKeyTodos))]
+		public void Update_WithMismatchKey_ShouldReturnBadRequest(string key, TodoItem item)
+		{
+			var todoRepository = new MockTodoRepository();
+			var todoController = new TodoController(todoRepository);
+
+			var result = todoController.Update(key, item);
+			Assert.IsType(typeof (BadRequestResult), result);
+		}
+
+		[Theory, MemberData(nameof(MissingKeyTodos))]
+		public void Update_WithMissingItem_ShouldReturnNotFound(string key, TodoItem item)
+		{
+			var todoRepository = new MockTodoRepository();
+			var todoController = new TodoController(todoRepository);
+
+			var result = todoController.Update(key, item);
+			Assert.IsType(typeof(NotFoundResult), result);
+		}
+
+		[Theory]
+		[InlineData("-1")]
+		[InlineData("asdf")]
+		[InlineData("foobar")]
+		public void Delete_WithWrongId_ShouldReturnNotFound(string key)
+		{
+			var todoRepository = new MockTodoRepository();
+			var todoController = new TodoController(todoRepository);
+
+			var result = todoController.Delete(key);
+			Assert.IsType(typeof(NotFoundResult), result);
+		}
+
+		[Theory]
+		[InlineData("1")]
+		[InlineData("2")]
+		[InlineData("3")]
+		public void Delete_WithId_ShouldReturnNoContent(string key)
+		{
+			var todoRepository = new MockTodoRepository();
+			var todoController = new TodoController(todoRepository);
+
+			var result = todoController.Delete(key);
+			Assert.IsType(typeof(NoContentResult), result);
+		}
+	}
+}
